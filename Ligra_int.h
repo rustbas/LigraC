8471d3dd9bf3d05829636734b34ee084c9f2d0e9
rustbas <hrustbas@gmail.com>
2024-01-02 14:45:34 +0000

    init header lib file for int version

---
+++ Ligra_int.h
@@ -0,0 +1,41 @@
+typedef struct {
+    unsigned int n;
+    unsigned int m;
+    int** MM;
+} MM_i;
+
+MM_i zeroMM_i(unsigned int n, unsigned int m) {
+
+    MM_i res;
+    res.n = n;
+    res.m = m;
+
+    res.MM = (int**) malloc(n * sizeof(int*));
+    for (int i=0; i<n; i++) {
+        res.MM[i] = (int*) malloc(m * sizeof(int));
+    }
+
+    return res;
+}
+
+MM_i add_constMM_i(MM_i MM, int C) {
+    for (int i=0; i<MM.n; i++) {
+        for (int j=0; j<MM.m; j++) {
+            MM.MM[i][j] += C;
+        }
+    }
+
+    return MM;
+}
+
+void printMM_i(MM_i MM) {
+    unsigned int n = MM.n;
+    unsigned int m = MM.m;
+
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<m; j++) {
+            printf("%d ", MM.MM[i][j]);
+        }
+        printf("\n");
+    }
+}
