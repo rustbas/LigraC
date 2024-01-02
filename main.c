4feeef51ec71f858f8864479f303394b3b3a585b
rustbas <hrustbas@gmail.com>
2024-01-02 14:48:36 +0000

    add testing operators

--- main.c
+++ main.c
@@ -3,8 +3,21 @@
 #include <stdio.h>
 #endif
 
+#ifndef STDLIB_H
+#define STDLIB_H
+#include <stdlib.h>
+#endif
+
+#include "Ligra_int.h"
+
 int main(int argc, char** argv) {
-    printf("Hello, world!\n");
+    MM_i test = zeroMM_i(5, 5);
+
+    printMM_i(test);
+
+    add_constMM_i(test, -3);
+
+    printMM_i(test);
 
     return 0;
 }
