a1eb4b0575a7bcec1fd913addd52a19b101231b4
rustbas <hrustbas@gmail.com>
2024-01-03 11:55:57 +0000

    tests

--- main.c
+++ main.c
@@ -8,17 +8,31 @@
 #include <stdlib.h>
 #endif
 
+#ifndef TIME_H
+#define TIME_H
+#include <time.h>
+#endif
+
 #include "Ligra_int.h"
 
 int main(int argc, char** argv) {
-    MM_i test = randMM_i(5, 25, 0, 10);
+    srand(time(0));
 
-    printMM_i(test);
+    MM_i test1 = randMM_i(1, 3, -3, 3);
+    MM_i test2 = randMM_i(2, 2, 0, 10);
+    MM_i test3 = eyeMM_i(5);
 
-    add_constMM_i(test, -3);
-
+    printMM_i(test1);
     printf("\n");
-    printMM_i(test);
+    /*printMM_i(test3);*/
+    printf("\n");
+    /*printMM_i(mulMM_i(test1, test3));*/
+
+    printMM_i(mulConstMM_i(test1, 5));
+
+    freeMM_i(test1);
+    freeMM_i(test2);
+    freeMM_i(test3);
 
     return 0;
 }
